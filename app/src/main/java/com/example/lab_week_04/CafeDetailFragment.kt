b06d89cd0bd3c72ec5aba824d3fccc5a45d761ca
Winson <winsonsovian2@gmail.com>
2025-09-22 01:31:21 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -5,29 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
+private const val TAB_CONTENT = "TAB_CONTENT"
+private const val ARG_TITLE = "ARG_TITLE"
+private const val ARG_DESC = "ARG_DESC"
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CafeDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CafeDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var title: String? = null
+    private var desc: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            title = getString(it.getInt(ARG_TITLE))
+            desc = getString(it.getInt(ARG_DESC))
         }
     }
 
@@ -35,26 +27,21 @@ class CafeDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.cafe_title)?.text = title
+        view.findViewById<TextView>(R.id.cafe_desc)?.text = desc
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CafeDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(titleRes: Int, descRes: Int) =
             CafeDetailFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(ARG_TITLE, titleRes)
+                    putInt(ARG_DESC, descRes)
                 }
             }
     }
